f515892a06947b2b2c43694553a77a8b2a084f48
khpolash <khpolashbd@gmail.com>
2024-02-17 17:04:52 +0600

    Solve problem 1371A Magical Sticks

---
+++ MagicalSticks/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1371A Magical Sticks!\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    long n = long.Parse(Console.ReadLine()!);
+    long result = n % 2 == 0 ? (n / 2) : 1 + ((n - 1) / 2);
+    Console.WriteLine(result);
+}

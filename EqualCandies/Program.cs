30ff1dbd71ad05e9a562ffe852082a8a68fe98f6
khpolash <khpolashbd@gmail.com>
2024-02-17 17:24:36 +0600

    Solve problem 1676B Equal Candies

---
+++ EqualCandies/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1676B Equal Candies!\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine()!);
+
+    int[] candies = new int[n];
+    string[] inputs = Console.ReadLine()!.Split();
+
+    for (int j = 0; j < n; j++)
+    {
+        candies[j] = int.Parse(inputs[j]);
+    }
+
+    int min = candies.Min();
+
+    int sum = candies.Sum(element => element - min);
+
+    Console.WriteLine(sum);
+}

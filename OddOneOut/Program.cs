f15668cedfe754c474b9d0b0ef2f551459335d8d
khpolash <khpolashbd@gmail.com>
2024-02-17 21:22:10 +0600

    Solve problem 1915A Odd One Out!

---
+++ OddOneOut/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1915A Odd One Out!\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    int[] inputs = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+    int uniqueOne = inputs.GroupBy(x => x).Where(x => x.Count() == 1).Select(x => x.Key).FirstOrDefault();
+
+    Console.WriteLine(uniqueOne);
+
+}

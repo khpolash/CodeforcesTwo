6e9f502f915cd1a23e31da436c7311aeb815bb79
khpolash <khpolashbd@gmail.com>
2024-02-18 15:22:07 +0600

    Solve problem 1772A A+B?

---
+++ APlusB/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1772A A+B!\n");
+
+int t = int.Parse(Console.ReadLine()!);
+for (int i = 0; i < t; i++)
+{
+    string inputs = Console.ReadLine()!;
+
+    int a = int.Parse(inputs[0].ToString());
+    int b = int.Parse(inputs[2].ToString());
+
+    Console.WriteLine(a + b);
+}

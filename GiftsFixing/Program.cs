b05dc30c222aa54f76b146844c31f1a187f4a3a4
khpolash <khpolashbd@gmail.com>
2024-02-18 16:21:57 +0600

    Solve problem 1399B Gifts Fixing!

---
+++ GiftsFixing/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1399B Gifts Fixing!\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine()!);
+    string[] strCandies = Console.ReadLine()!.Split();
+    string[] strOranges = Console.ReadLine()!.Split();
+
+    long[] candies = new long[n];
+    long[] oranges = new long[n];
+
+    for (int j = 0; j < n; j++)
+    {
+        candies[j] = int.Parse(strCandies[j]);
+        oranges[j] = int.Parse(strOranges[j]);
+    }
+
+    long minCandy = candies.Min();
+    long minOrange = oranges.Min();
+
+    long moves = 0;
+
+    for (int k = 0; k < n; k++)
+    {
+        moves += Math.Max(candies[k] - minCandy, oranges[k] - minOrange);
+    }
+
+    Console.WriteLine(moves);
+}

6777124438054aa6637b67726c57bcaa83124b90
khpolash <khpolashbd@gmail.com>
2024-02-18 10:01:32 +0600

    rearrange code

--- OddOneOut/Program.cs
+++ OddOneOut/Program.cs
@@ -6,9 +6,6 @@ int t = int.Parse(Console.ReadLine()!);
 for (int i = 0; i < t; i++)
 {
     int[] inputs = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
-
     int uniqueOne = inputs.GroupBy(x => x).Where(x => x.Count() == 1).Select(x => x.Key).FirstOrDefault();
-
     Console.WriteLine(uniqueOne);
-
 }

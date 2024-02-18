8f08636c1eedd6ff9dad9baf98efbfa37c266326
khpolash <khpolashbd@gmail.com>
2024-02-18 14:48:17 +0600

    Solve problem 959A Mahmoud and Ehab and the even-odd game

---
+++ MahmoudAndEhabAndTheEvenOddGame/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("959A Mahmoud and Ehab and the even-odd game!\n");
+
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine((n % 2 == 0) ? "Mahmoud" : "Ehab");

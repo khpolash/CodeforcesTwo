2756c0bc192bfdf7518a2d9fa0fb95f3afe7500b
khpolash <khpolashbd@gmail.com>
2024-02-18 15:13:31 +0600

    Solve problem 1829A Love Story

---
+++ LoveStory/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1829A Love Story!\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+char[] s = "codeforces".ToCharArray();
+
+for (int i = 0; i < t; i++)
+{
+    char[] input = Console.ReadLine()!.ToLower().ToCharArray();
+    int differences = s.Zip(input, (a, b) => a != b ? 1 : 0).Sum();
+    Console.WriteLine(differences);
+}

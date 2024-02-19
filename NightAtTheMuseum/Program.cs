f23d7c5a2c3d3eaa4362a00b9aa9603a420615cc
khpolash <khpolashbd@gmail.com>
2024-02-19 22:34:15 +0600

    Solve problem 731A Night at the Museum!

---
+++ NightAtTheMuseum/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("731A Night at the Museum!\n");
+
+string input = Console.ReadLine()!.ToLower()!;
+
+char start = 'a';
+long sum = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    long x = Math.Abs(input[i] - start);
+    long y = (26 - x);
+    sum += Math.Min(x, y);
+    start = input[i];
+}
+
+Console.WriteLine(sum);

b3a0e8ef5e7ac485bcc1af8d4d29e4ae4d37102b
khpolash <khpolashbd@gmail.com>
2024-02-20 11:25:17 +0600

    Solve problem 1873B Good Kid!

---
+++ GoodKid/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1873B Good Kid!\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine()!);
+    string[] inputs = Console.ReadLine()!.Split();
+    int[] ints = new int[n];
+
+    for (int j = 0; j < n; j++)
+    {
+        ints[j] = int.Parse(inputs[j]);
+    }
+    Array.Sort(ints);
+    ints[0]++;
+
+    long result = ints.Aggregate((a, b) => a * b);
+
+    Console.WriteLine(result);
+}

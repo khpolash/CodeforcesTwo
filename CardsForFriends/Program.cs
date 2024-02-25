ba827684fa7cfc5d5eae5a16ddfb653e8abedb9f
khpolash <khpolashbd@gmail.com>
2024-02-25 10:33:26 +0600

    solve problem 1472A Cards for Friends!

---
+++ CardsForFriends/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1472A Cards for Friends!\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    string[] str = Console.ReadLine()!.Split();
+    int w = int.Parse(str[0]);
+    int h = int.Parse(str[1]);
+    long n = long.Parse(str[2]);
+
+    long result = 1;
+
+    while (w % 2 == 0)
+    {
+        w /= 2;
+        result *= 2;
+    }
+    while (h % 2 == 0)
+    {
+        h /= 2;
+        result *= 2;
+    }
+
+    Console.WriteLine(result >= n ? "YES" : "NO");
+}

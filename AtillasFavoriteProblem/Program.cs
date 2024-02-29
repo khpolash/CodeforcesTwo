7cf1322e6bcf97c77cdbad2be971e16dc7d8e9a1
khpolash <khpolashbd@gmail.com>
2024-02-29 10:14:52 +0600

    solve problem 1760B Atilla's Favorite Problem

---
+++ AtillasFavoriteProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1760B Atilla's Favorite Problem!\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine()!);
+    string str = Console.ReadLine()!.ToLower();
+    char[] chars = new char[n];
+    chars = str.ToCharArray();
+    Array.Sort(chars);
+    int result = GetAlphabetPosition(chars[^1]);
+    Console.WriteLine(result);
+}
+
+static int GetAlphabetPosition(char letter)
+{
+    return letter - 'a' + 1;
+}

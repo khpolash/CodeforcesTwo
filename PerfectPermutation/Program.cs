9acb1d4f06312dff60663a22578e100716b7a289
khpolash <khpolashbd@gmail.com>
2024-03-04 16:07:49 +0600

    Solve problem 233A Perfect Permutation

---
+++ PerfectPermutation/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("233A Perfect Permutation!\n");
+
+int n = int.Parse(Console.ReadLine()!);
+
+if (n % 2 == 1)
+{
+    Console.WriteLine("-1");
+    return;
+}
+
+for (int i = 1; i <= n; i += 2)
+{
+    Console.Write($"{i + 1} {i} ");
+}
+

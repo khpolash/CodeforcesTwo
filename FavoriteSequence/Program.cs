94aa0315d990177e820e9b5163a64262db959669
khpolash <khpolashbd@gmail.com>
2024-03-04 16:47:36 +0600

    Solve problem 1462A Favorite Sequence!

---
+++ FavoriteSequence/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1462A Favorite Sequence!\n");
+
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    long n = long.Parse(Console.ReadLine()!);
+    string[] str = Console.ReadLine()!.Split();
+
+    long[] numArr = new long[n];
+    long[] resultArr = new long[n];
+
+    for (int j = 0; j < n; j++)
+    {
+        numArr[j] = int.Parse(str[j]);
+    }
+
+    for (int k = 0; k < n; k++)
+    {
+        if (k % 2 == 0)
+        {
+            resultArr[k] = numArr.First();
+            numArr = numArr.Skip(1).ToArray();
+        }
+        else
+        {
+            resultArr[k] = numArr.Last();
+            numArr = numArr.SkipLast(1).ToArray();
+        }
+
+    }
+    Console.WriteLine(string.Join(" ", resultArr));
+}

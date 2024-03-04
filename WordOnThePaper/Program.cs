c1d3881b24f2405e0754fb942230823476fa9d3e
khpolash <khpolashbd@gmail.com>
2024-03-04 17:19:13 +0600

    Solve problem 1850C Word on the Paper

---
+++ WordOnThePaper/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1850C Word on the Paper!\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    string result = string.Empty;
+    for (int j = 0; j < 8; j++)
+    {
+        string str = Console.ReadLine()!;
+        result += new string(str.Where(char.IsLetter).ToArray());
+    }
+    Console.WriteLine(result);
+}

e2f7b2d18f2f3e3499e35f02b7aafc49d72cdc62
khpolash <khpolashbd@gmail.com>
2024-03-05 10:55:06 +0600

    Solve problem 1353A Most Unstable Array

---
+++ MostUnstableArray/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1353A Most Unstable Array!\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    long[] numArr = Console.ReadLine()!.Split().Select(long.Parse).ToArray();
+
+    Console.WriteLine(Math.Min(2, numArr[0] - 1) * numArr[1]);
+}

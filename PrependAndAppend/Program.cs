cdf5e20b8a704e3c93336161fc08e38756d9bd60
khpolash <khpolashbd@gmail.com>
2024-03-05 13:14:01 +0600

    Solve problem 1791C Prepend and Append

---
+++ PrependAndAppend/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1791C Prepend and Append!\n");
+
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine()!);
+    string s = Console.ReadLine()!;
+    int l = 0, r = n - 1, ans = n;
+
+    while (s[l] != s[r] && ans > 0)
+    {
+        l++; r--; ans -= 2;
+    }
+
+    Console.WriteLine(ans);
+}

bb477ebec09c68a8e736dd5e82d2b291085c6a01
khpolash <khpolashbd@gmail.com>
2024-03-05 10:48:35 +0600

    Solve problem 1454A Special Permutation!

---
+++ SpecialPermutation/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1454A Special Permutation!\n");
+
+int t = int.Parse(Console.ReadLine()!);
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine()!);
+    int[] res = new int[n];
+    for (int j = 0; j < n; j++)
+    {
+        res[j] = ((j + 1) % n) + 1;
+    }
+
+    Console.WriteLine(string.Join(" ", res));
+}

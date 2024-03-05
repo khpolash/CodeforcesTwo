5883bc91a999a50597cfeb1f883a19dde9d6f461
khpolash <khpolashbd@gmail.com>
2024-03-05 10:24:37 +0600

    Solve problem 1807b Grab the Candies!

---
+++ GrabTheCandies/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1807b Grab the Candies!\n");
+
+int t = int.Parse(Console.ReadLine()!);
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine()!);
+    int[] numArr = new int[n];
+    string[] str = Console.ReadLine()!.Split();
+
+    for (int j = 0; j < n; j++)
+    {
+        numArr[j] = int.Parse(str[j]);
+    }
+
+    int sumEven = 0, sumOdd = 0;
+
+    for (int k = 0; k < n; k++)
+    {
+        if (numArr[k] % 2 == 0)
+        {
+            sumEven += numArr[k];
+        }
+        else
+        {
+            sumOdd += numArr[k];
+        }
+    }
+
+    Console.WriteLine(sumEven > sumOdd ? "YES" : "NO");
+}

3d9cd7d5277542a333899c9221cfbbde9428812d
khpolash <khpolashbd@gmail.com>
2024-05-06 10:18:33 +0600

    add new project

---
+++ BlankSpace/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1829B Blank Space!\n");
+
+int t = int.Parse(Console.ReadLine()!);
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine()!);
+    string[] str = Console.ReadLine()!.Split();
+    int current = 0, ans = 0;
+
+    for (int j = 0; j < n; j++)
+    {
+        if (int.Parse(str[j]) == 0)
+        {
+            current++;
+        }
+        else
+        {
+            ans = Math.Max(ans, current);
+            current = 0;
+        }
+    }
+    Console.WriteLine(Math.Max(ans, current));
+}
